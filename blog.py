ebab9ec607596cba05bbcf64d70db22ea45cd837
kousu <nick@kousu.ca>
2015-12-19 20:03:24 -0500

    Add login/logout so I can test the ACLs, and commit the templates I forgot about

--- blog.py
+++ blog.py
@@ -10,10 +10,10 @@ from flask_loginless import *
 from flask_acl import *
 
 import os, time
-
+import datetime
 
 app = Flask(__name__)
-app.secret_key = os.urandom(50)
+app.secret_key = "bloggy" #os.urandom(50)
 
 lm = LoginManager(app)
 
@@ -21,8 +21,10 @@ acl = ACL(app)
 
 class User(LoginTokenMixin, UserMixin):
 	@classmethod
-	def reload(cls, id, display_name, creation_date):
-		return cls(id, display_name, creation_date)
+	def reload(cls, id):
+		display_name = id.title()
+		#creation_date = datetime.datetime.now()
+		return cls(id, display_name)
 	
 	def __init__(self, id, display_name=None):
 		self.id = id
@@ -30,8 +32,7 @@ class User(LoginTokenMixin, UserMixin):
 		self.display_name = display_name
 		self.creation_date = int(time.time())
 
-
-
+lm.user_loader(User.reload)
 
 BLOGTITLE = "Kousu's Magical Fantasy Land"
 
@@ -39,8 +40,10 @@ BLOGTITLE = "Kousu's Magical Fantasy Land"
 # The limitation of unix permissions is that each file has *exactly one* group it can be in
 #  and these groups are
 #   - not user editable
-#   - 
-owner = {"kousu"}
+#   - not composable
+#  so there's no way to express things like "give to all salespeople except those who are overseas" or "show to everyone on my friends list except my boss"
+# they work alright to express capabilities, things like "can use the soundcard" "can use the video card" "can tweak network settings", so long as each of these are totally independent
+admins = {"kousu"}
 family = {"sister", "brother", "mom", "dad", "aunt"}
 friends = {"oauth:https://facebook.com/sally", "oauth:https://facebook.com/sanchez", "mailto:jack@gmail.com"}
 
@@ -52,18 +55,36 @@ def index():
 
 
 # TODO:
-#@app.route("/post/<path:path>")
-##@acl(lambda user, path: check_file_acl(user, path))
-#def view_post(path):
-#	return render_template('post.html', blogtitle=BLOGTITLE, title=path, content=Markup(open(os.path.join("_posts", path)).read()), comments=[])
+#  the idea is that some pages (/index, /manage) will have fixed permissions coded at the app level
+#  but for everything under /post, we delegate to the external, user-controlled, database
+@app.route("/post/<path:path>")
+#@acl(lambda user, path: database_acl(user, path))
+@acl.public
+def view_post(path):
+	return render_template('post.html', blogtitle=BLOGTITLE, title=path,
+		#content=Markup(open(os.path.join("_posts", path)).read()),
+		comments=[])
 
+
+# things I could do:
+# - make acl take EITHER a set or a predicate
+# - make ACL know about sets (this is probably the most common use
+# - 
 
 @app.route("/post/family")
 @acl(lambda user: user.get_id() in family)
 def view_post_family():
 	return render_template('post.html', blogtitle=BLOGTITLE, title="Family-Accessible Post")
 
+# in flask, an "endpoint" is a unique string + function
+# usually the unique string is function.__name__, but this can (argh) be overridden
+# I want to attach ACLs to functions, but in Flask that means attaching them to endpoints
+# request.url_rule.endpoint gets the currently active endpoint string (after parsing and routing happens)
+# (and request.view_args gets the values for any variables marked by <varname> syntax in the route string)
+# 
+
 @app.route("/post/friends")
+#@acl.allow(admins)  #maybe let the ACLs stack? 
 @acl(lambda user: user.get_id() in friends)
 def view_post_friends():
 	return render_template('post.html', blogtitle=BLOGTITLE, title="Friend-Accessible Post")
@@ -76,9 +97,40 @@ def view_post_public():
 
 @app.route("/post/default")
 def view_post_default():
-	return render_template('post.html', blogtitle=BLOGTITLE, title="Default-Perms Post")
+	return render_template('post.html', blogtitle=BLOGTITLE, title="Default-Perms Post", content="This should be inaccessible to everyone. Except maybe the owner. I guess. But then we'd have to give Flask-ACL a notion of owner groups...")
 
 
+@app.route("/manage")
+@acl(lambda user: user.get_id() in admins)
+def manage():
+	if request.method == "POST":
+		# ... handle stuff
+		# sending a redirect should skip the "Do You Want To Resubmit This Form?" behaviour? I think?
+		return redirect(url_for("manage"))
+	return render_template('manage.html')
+
+
+# XXX DEBUG: this lets anyone login as anyone. It's for *TESTING* only. It's a giant, obvious, backdoor.
+@app.route("/login/<user_id>")
+@acl.public
+def login(user_id):
+	login_user(app.login_manager.user_callback(user_id))
+	app.logger.debug("Logged in %s as %s", user_id, current_user.get_id())
+	return redirect(url_for("index"))
+
+@app.route("/logout")
+@acl.public
+def logout():
+	logout_user()
+	return redirect(url_for("index"))
+
+
+#Inspiration: petnames
+# http://www.skyhunter.com/marcs/petnames/IntroPetNames.html
+# this isn't quite a petnames system, but it does exploit:
+# - linking keys to shorter names
+# - storing the mappings decentralized
+# 
 
 if __name__ == '__main__':
 	if __debug__:
--- flask_acl.py
+++ flask_acl.py
@@ -7,33 +7,56 @@ from flask.ext.login import current_user
 from functools import wraps
 
 
-
+# Okay so question:
+# first: this thing doesn't necessarily need to come as a class, does it?
+# second: should we store the permission statefully?
+# third: where the arguments at?
 
 class ACL(object):
 	"""
 	Framework for Access Control Lists for Flask.
+	# XXX maybe this should be called "Permissions"?
 	Works with Flask-Login
 	
 	app = Flask(__name__)
 	acl = ACL(app)
 	
 	#@acl
+	# For now, reading and writing permissions are not distinguished, to keep the implementation simple.
+	#  if you want to have a single page that has different permissions for readers and writers
+	# you should split it into two methods with different methods:
+	@app.route("/account", methods=["GET"])
+	@acl(...readers...)
+	def read_account(....): ...
 	
-	When you set an ACL object on your app, every view defaults to private.
-	To allow access, you must explicitly allow.
+	@app.route("/account", methods=["POST"])
+	@acl(...writers...)
+	def write_account(....): ...
+	
+	To get the full benefit of this system, you should rearrange your app so that each distinct action is isolated to a single route
+	and then ACL them all separately
+	so that each route can be, e.g. /manage/<user>
+	
+	When you set an ACL object on your app, every view defaults
+	to private (i.e. permissions fail closed). You must explicitly
+	mark every view with an ACL rule, even if it's just @ACL.public
 	"""
 	def __init__(self, app):
 		if not hasattr(app, 'login_manager'):
 			raise ValueError("You must set a Flask-Login LoginManager on your app before using Flask-ACL")
 		
 		self._app = app
-		#self._app.before_request(self._check_acl)
+		self._app.before_request(self._check_acl)
 		# ---> TODO: default all apps to 
 	
-	def _check_acl(*args, **kwargs):
-		self._app.logger.debug("_check_acl: %s, %s", args, kwargs)
-		if current_user.id == "unauthed":
-			return abort(401)
+	def _check_acl(self, *args, **kwargs):
+		self._app.logger.debug("_check_acl: %s, %s", request.method, request.path)
+		self._app.logger.debug("_check_acl: args=%s, kwargs=%s", args, kwargs)
+		self._app.logger.debug("_check_acl: endpoint: %s, viewargs = %s", request.url_rule.endpoint, request.view_args)
+		# Do I need to attach
+		
+		# I want to hook 
+		#return abort(401)
 	
 	def private(self, view):
 		"Make the given view totally public. i.e. skip ACL checking and just deny"
@@ -41,7 +64,7 @@ class ACL(object):
 		def decorated_view(*args, **kwargs):
 			return abort(401)
 		return decorated_view
-
+	
 	def public(self, view):
 		"Make the given view totally public. i.e. disable ACL checking"
 		@wraps(view)
